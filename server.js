b2d2cc06b65b4fcefb5c6e92ec32772f5448b737
SoulReaper06 <johnatj33@gmail.com>
2024-04-28 15:59:00 +1000

    added socket

--- server.js
+++ server.js
@@ -1,48 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const http = require('http');
+const { Server } = require('socket.io');
+
 const adopterController = require('./controllers/adopterController');
 const petController = require('./controllers/petController');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const PORT = process.env.PORT || 3000;
 
-
 const mongoURI = "mongodb+srv://jonatjmathew08:zffmrBVwaGL0LnFS@deakin.mocsaum.mongodb.net/?retryWrites=true&w=majority&appName=deakin"
-
 
 async function connectDb() {
   try {
     await mongoose.connect(mongoURI);
     console.log("You successfully connected to MongoDB!");
-  } finally {
-    // await client.close();
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
   }
 }
 connectDb().catch(console.dir);
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
 
 app.post('/submitAdopter', adopterController.submitAdopter);
 app.post('/submitPet', petController.submitPet);
 app.get('/api/pets', petController.getPets);
 
-app.get('/addTwonumbers/:firstNumber/:SecondNumber', function(req, res, next) {
-  var firstNumber = parseInt(req.params.firstNumber);
-  var SecondNumber = parseInt(req.params.SecondNumber);
-
-  if (isNaN(firstNumber) || isNaN(SecondNumber)) {
-      res.status(400).json({ error: "Both should be a number" });
-  } else {
-      var result = firstNumber + SecondNumber;
-      res.status(200).json({ result: result });
-  }
-});
-
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('send-message', (message) => {
+    console.log('Message received:', message);
+    io.emit('receive-message', message);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-module.exports = app;
+module.exports = { app, server };
